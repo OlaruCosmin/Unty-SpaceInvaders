ba8bc932c7ce219e159831fe654035e3fca9023f
OlaruCosmin <olaru.cosmin.al@gmail.com>
2019-01-17 12:26:20 +0200

    First Commit
    Added unity project to GitHub

---
+++ Space Invaders/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ScoreDisplay : MonoBehaviour {
+
+    Text scoreText;
+    GameSesion gameSesion;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        gameSesion = FindObjectOfType<GameSesion>();
+    }
+
+    void Update()
+    {
+        scoreText.text = gameSesion.GetScore().ToString();
+    }
+
+}
---
+++ Space Invaders/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject {
+
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFator = 0.3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab(){return this.enemyPrefab;}
+    public List<Transform> GetWaypoints()
+    {
+        var waveWaypoints = new List<Transform>();
+        foreach(Transform child in pathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints;
+    }
+    public float GetTimeBetweenSpawns(){return this.timeBetweenSpawns;}
+    public float GetSpawnRandonFactor(){return this.spawnRandomFator;}
+    public int GetNumberOfEnemies(){return this.numberOfEnemies;}
+    public float GetMoveSpeed(){return this.moveSpeed;}
+
+
+    
+
+}
